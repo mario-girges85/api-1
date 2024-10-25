504ca9b62ff25afdb2aaa00e3907025bd5dc8b41
Hazem.Solima.devn <hazem.soliman.dev@gmail.com>
2024-10-25 16:13:06 +0300

    test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 mongoose

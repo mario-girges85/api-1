119d9e95f24b4c0d9a9702290c1ac981b09fc77e
Hazem.Solima.devn <hazem.soliman.dev@gmail.com>
2024-10-25 13:52:22 +0300

    edit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
-import multer from "multer";
 import dotenv from "dotenv";
 import Products from "./routes/product.js";
 import User from "./routes/user.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
@@ -21,6 +24,10 @@ mongoose
   .catch((err) => {
     console.log("Failed to connect to MongoDB", err);
   });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html"); 
+});
 
 app.use("/products", Products);
 app.use("/users", User);

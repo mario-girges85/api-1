0846ecf690e1b29102f1d89ecc321610fa59c2fd
mario-girges85 <mario85.girges@gmail.com>
2024-10-26 12:51:49 -0700

    0

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
-app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Only allow requests from this origin
-  })
-);
+app.use(cors("*"));
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
